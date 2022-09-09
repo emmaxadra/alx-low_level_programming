076f843d353cf21e8fa0e46f023137ee4a9d1872
emmaxadra <emmaxadra@gmail.com>
2022-09-09 05:32:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - prints a proper grammar
+ * main - prints a proper grammar
  *
  * Description:using the main function
  * this peogram prints "proper grammar, but the outcome is a piece of art
