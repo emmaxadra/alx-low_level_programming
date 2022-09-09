002d1d3361d566ec47b758f35e85547f63243102
emmaxadra <emmaxadra@gmail.com>
2022-09-09 10:03:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - printing the starting function
+ *main - printing the starting function
  *
  * Return:0 (successful) 
  */
 int main(void)
 {
-	printf("with proper grammar but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,")
 	return (0);
 } 
