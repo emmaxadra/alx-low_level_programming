bcbf163d6bb5af11e4f4947ddae50901b0e3e8f9
emmaxadra <emmaxadra@gmail.com>
2022-09-09 05:24:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - prints a proper grammar
+ *
+ * Description:using the main function
+ * this peogram prints "proper grammar, but the outcome is a piece of art
+ * Reture: 0
+ */
+int main(void)
+{
+	printf("proper grammar, but the outcome is a piece of art");
+	reture (0);
+}
