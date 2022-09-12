5d4c3ee00c14aef88f18a9753c932293c4a311e4
emmaxadra <emmaxadra@gmail.com>
2022-09-12 07:21:03 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: main function
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
