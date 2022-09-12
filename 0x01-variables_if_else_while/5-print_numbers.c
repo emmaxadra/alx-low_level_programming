1e36097677c8a05ce9a6403fb1912d62d63faa9a
emmaxadra <emmaxadra@gmail.com>
2022-09-12 07:01:00 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: main function
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
