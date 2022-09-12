a3fa90dd21ee18550b2a19e64d0188ff2b07414c
emmaxadra <emmaxadra@gmail.com>
2022-09-12 00:46:46 -0700

    run

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
 /**
- * mains - print the sizeof many var type
+ *main-printing C program size
+ *
+ * Description: this program print various types of computer
  *
  * Return: 0
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("size of a char: %lu byte(s)\n", sizeof(a));
-	printf("size of int: %lu byte(s)\n", sizeof(b));
-	printf("size of a long int: %lu byte(s)\n", sizeof(c));
-	printf("size of long long int: %lu byte(s)\n", sizeof(d));
-	printf("size of a float: %lu byte(s)\n", sizeof(e));
+	printf("size of char: %d byte(s)\n", sizeof(char));
+	printf("size of int: %d byte(s)\n", sizeof(int));
+	printf("size of long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
 
