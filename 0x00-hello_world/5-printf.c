e9b8e92044f0bae76cc8e6563a7c33ffbfa60889
emmaxadra <emmaxadra@gmail.com>
2022-09-12 00:01:53 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print the proper grammar
+ * main - printing proper grammar
  *
- * Description: using main function
- * 
+ * Description: this program output will print a print a proper grammar
+ *
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,");
-	return (0);
-} 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
+
