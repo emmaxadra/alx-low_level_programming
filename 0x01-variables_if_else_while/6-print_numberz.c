3bab4605084c9eec92856681791e7de477e76cbd
emmaxadra <emmaxadra@gmail.com>
2022-09-12 07:11:53 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: main function
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10; a++)
+		putchar('0' + a);
+	putchar('\n');
+	return (0);
+}
