e23d352226838abcf4c525e7d4e87b9a0dd44529
emmaxadra <emmaxadra@gmail.com>
2022-09-13 11:30:14 -0700

    fkajak

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ *
+ * Description: writing a program
+ *
+ * @n: integer to make absolute
+ *
+ * Return: int or int time negative
+ *
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
