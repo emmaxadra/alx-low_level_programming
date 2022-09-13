7065883bddc304a0f78de93f5ee4343768986a89
emmaxadra <emmaxadra@gmail.com>
2022-09-13 01:40:17 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main -check the code
+ *
+ * Description: main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	char h;
+
+	for (h = 0; h <= 9; i++)
+	{
+		for (i = 0; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
+
