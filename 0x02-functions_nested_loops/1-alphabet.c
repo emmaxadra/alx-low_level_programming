cc920863bc0aa244e7c5c108d1d51e171e83a469
emmaxadra <emmaxadra@gmail.com>
2022-09-13 01:12:46 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Description: print alphabet
+ *
+ * Return: 0
+ *
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
