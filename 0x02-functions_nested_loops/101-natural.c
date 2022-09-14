ab05f1c144b84452e5d2809d179083d1e0bf5999
emmaxadra <emmaxadra@gmail.com>
2022-09-14 00:55:53 -0700

    wkqja

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Descriprion: main function
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int a;
+	int b = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			b += a;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
