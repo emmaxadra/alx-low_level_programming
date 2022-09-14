e840f1787feeb86387029716619e2eea116636c7
emmaxadra <emmaxadra@gmail.com>
2022-09-13 19:47:01 -0700

    daa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,15 @@
  *
  * Description: print_alphabet_x10 times
  *
- * Return: 0
+ * Return: 0;
+ *
  */
 void print_alphabet_x10(void)
 {
 	char i;
 	char h;
 
-	for (h = 0; h <= 9; i++)
+	for (h = 0; h <= 9; h++)
 	{
 		for (i = 0; i <= 'z'; i++)
 		{
