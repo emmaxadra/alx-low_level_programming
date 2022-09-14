f73d9b35fdad39ca24e7e61063dc737b40ed8927
emmaxadra <emmaxadra@gmail.com>
2022-09-14 15:19:33 -0700

    catt

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -7,6 +7,5 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-void print_times_table(int n);
 
-#endif
+#endif /* MAIN_H */
