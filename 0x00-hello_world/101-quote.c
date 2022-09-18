fcf0ae3b0b71e7e067623a160e483a118bc68039
emmaxadra <emmaxadra@gmail.com>
2022-09-18 07:03:05 -0700

    ahajj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,13 +6,13 @@
  *
  * Description: main function
  *
- * Return: always 1
+ * Return: always 1 (success)
  *
  */
 int main(void)
 {
 	write(2,
 
-and that piece of art is useful" - Dora Korpar, 2015-10-19,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
