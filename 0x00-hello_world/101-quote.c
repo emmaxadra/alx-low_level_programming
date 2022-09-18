02a807bafd7be5bcca38f6163ec17b411278e337
emmaxadra <emmaxadra@gmail.com>
2022-09-18 05:36:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,13 @@
  *
  * Description: main function
  *
- * Return: 0
+ * Return: always 1
  *
  */
 int main(void)
 {
 	write(2,
+
 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
 	return (1);
 }
