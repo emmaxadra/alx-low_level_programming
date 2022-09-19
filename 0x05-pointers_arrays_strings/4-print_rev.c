a241bf886665e5b5fd0c4fef7df0136d37859def
emmaxadra <emmaxadra@gmail.com>
2022-09-19 04:31:41 -0700

    aqiq

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the used string reference pointer
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len - strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
