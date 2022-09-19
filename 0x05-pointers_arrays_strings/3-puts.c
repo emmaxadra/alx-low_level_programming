34fdbe84bbcea5d084d202c1266ca78c7c68341b
emmaxadra <emmaxadra@gmail.com>
2022-09-19 04:05:03 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string and a new line
+ * @str: pointer to a string to print
+ *
+ * Description: prints a string
+ * on success: return no error
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
