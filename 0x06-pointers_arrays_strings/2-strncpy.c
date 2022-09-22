c03ff382da9ff7c2d38c7131f045c31d3e80383c
emmaxadra <emmaxadra@gmail.com>
2022-09-21 19:01:11 -0700

    kaqk

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,7 +9,7 @@
  * @dest: buffer storing the string copy
  * @src: the soirce string
  * @n: max number of byte copied
- * Return: returns
+ * Return: a pointer to the resulting g string dest.
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -20,7 +20,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '/0'; i++)
 		dest[i] = src[i];
 
-	for (0; i < n; i++)
+	for ( ; i < n; i++)
 		dest[i] = '/0';
 
 	return (dest);
