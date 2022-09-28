f9a16d045f9aa4cb4249ca29cbb939c25e345151
emmaxadra <emmaxadra@gmail.com>
2022-09-28 11:59:47 -0700

    jaja

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+
 	else
-	{
-		_putchar(s[0]);
-	}
+		_putchar('\n');
 }
