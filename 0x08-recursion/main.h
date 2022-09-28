ac5232c7e65b6b90f07197c3f7f8f53e068e0d4a
emmaxadra <emmaxadra@gmail.com>
2022-09-28 04:59:14 -0700

    ajak

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 /**
  * int prototype()
  * void prototype()
  */
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -14,6 +17,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+void check(char *s, int start, int end, int *flag)
 
 #endif /*MAIN_H*/
 
