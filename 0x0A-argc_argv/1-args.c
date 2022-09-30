5ce91e26f3a8cf23dc3172bc403a4fe2ec49fe1f
emmaxadra <emmaxadra@gmail.com>
2022-09-30 05:11:06 -0700

    jaj

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -13,10 +13,10 @@ int main(int argc, char *argv[])
 	int i;
 
 	if (argc == 1)
-		printf("%d\n", agrc - 1);
+		printf("%d\n", argc - 1);
 	else
 	{
-		for (i = 0; *agrv; i++; agrv++)
+		for (i = 0; *argv; i++; argv++)
 			;
 
 		printf("%d\n", i - 1);
