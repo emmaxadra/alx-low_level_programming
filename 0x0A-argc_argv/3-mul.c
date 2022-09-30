19e03c2503640fa2e83e78d069ab17e5a2756182
emmaxadra <emmaxadra@gmail.com>
2022-09-30 08:06:47 -0700

    jaja

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Program that takes first two integer 
- * arguments and prints the product
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if not enough arguments passed in, 0 otherwise
+ * main - Prints the multiplication of two args numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
