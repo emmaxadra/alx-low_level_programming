fd092b46cb2be50bf219861c8b5f2a191eba825f
emmaxadra <emmaxadra@gmail.com>
2022-09-30 07:44:15 -0700

    anna

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,17 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++; argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
-
+	(void) argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
