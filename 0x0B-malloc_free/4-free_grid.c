ee9ecf43bb490ccf917f88053b5fbe4f7f42757c
emmaxadra <emmaxadra@gmail.com>
2022-10-03 01:35:35 -0700

    acs

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib h>
+
+/**
+ * free_grid - ...
+ * @grid: ...
+ * @height: ...
+ *
+ * Return: ...
+ */
+void free_grid(int **grid, int height)
+{
+	int index;
+
+	for (index = 0; index < height; index++)
+		free(grid[index]);
+
+	free(grid);
+}
