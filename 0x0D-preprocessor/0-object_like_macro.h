873e52c25a8b7dc244f008af60c2cd98f3b12cd6
emmaxadra <emmaxadra@gmail.com>
2022-10-10 10:13:01 -0700

    kak

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,6 +3,13 @@
 #include "0-object_like_macro.h"
 #include "0-object_like_macro.h"
 #include <stdio.h>
-#define Size 1024
+
+/**
+ * File: 0-object_like_macro.h
+ * Desc: Header file that defines a macro named
+ * SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
 
 #endif
