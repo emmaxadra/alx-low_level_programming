0610e28b7de9e4e280bda1c1f85f4e293b5d14c3
emmaxadra <emmaxadra@gmail.com>
2022-10-10 10:34:18 -0700

    akk

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,12 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  * main - Prints the name of the file of the program
- * was compiled from, followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	printf("%s\n", __file__);
-
 	return (0);
 }
