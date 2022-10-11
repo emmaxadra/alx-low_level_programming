11bfa6d30b29ad371c5e8610eb7e8fe77054ba06
emmaxadra <emmaxadra@gmail.com>
2022-10-11 08:53:56 -0700

    akka

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,12 @@
-#include _HEADER_
-#include _HEADER_
-
-/*
- * File: dog.h
- * Desc: Header file that defines a new type struct dog.
- */
+#ifndef STRTYP
+#define STRTYP
 
 /**
- * struct dog - A new type describing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: the struct is for a dog
  */
 struct dog
 {
@@ -19,13 +15,11 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /*STRTYP*/
