285d092c516abd08ebd0b4861337b443a187651b
emmaxadra <emmaxadra@gmail.com>
2022-10-11 09:48:08 -0700

    rheh

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog"
+#include <stdio.h>
+
+/**
+ * free_dog - frees the dog
+ * @d: the dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
