d10dfe3609fd412d85e16ca63386afef0c36bb7f
emmaxadra <emmaxadra@gmail.com>
2022-10-12 06:16:39 -0700

    oluea

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * array_iterator - Executes a function given as a
@@ -12,10 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 
-
-		while (size-- > 0)
-		{
-			action(*array);
-			array++;
-		}
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 }
