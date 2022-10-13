2616bda68655cd60d3f835ca7edfed41afad8f2f
emmaxadra <emmaxadra@gmail.com>
2022-10-13 02:22:14 -0700

    ajajak

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stblib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: seperator char
+ * @n: const var
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list printArgs;
+
+	va_start(printArgs, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0 && seperator != NULL)
+			printf("%s", seperator);
+		printf("%d", va_arg(printArgs, int));
+	}
+	va_end(printArgs);
+	printf("\n");
+}
