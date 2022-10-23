de47fac96eff5929ed90eb8773fc7b5e38bc007c
emmaxadra <emmaxadra@gmail.com>
2022-10-23 10:04:20 -0700

    jak

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * Auth: emmaxadra
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-more_malloc_free directory.
+ */
+
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
