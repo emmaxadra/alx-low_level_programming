d381809d0c86e6ee200ffa51a82a3d12d2b580bd
emmaxadra <emmaxadra@gmail.com>
2022-12-12 12:58:53 -0800

    hd

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/emmaxadra/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://raw.githubusercontent.com/emmaxadra/Alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
